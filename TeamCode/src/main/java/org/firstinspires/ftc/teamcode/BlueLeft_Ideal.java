099cd666270679c82fda0bceb9bd760be3bba904
adamzhang06 <reachadamzhang@gmail.com>
2023-12-03 13:30:57 -0600

    combined huskycolortest and huskytagtest into BlueLeft_Ideal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft_Ideal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft_Ideal.java
@@ -42,6 +42,7 @@ public class BlueLeft_Ideal extends LinearOpMode {
         } else {
             telemetry.addData(">>", "Press start to continue");
         }
+
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
         telemetry.update();
 
@@ -54,11 +55,16 @@ public class BlueLeft_Ideal extends LinearOpMode {
         //define trajectories
 
 
+
+
+
         //start is clicked
         waitForStart();
 
         if (isStopRequested()) return;
 
+        //start color detect (algorithm defined earlier)
+        //define x_pos
         int x_pos = -1;
 
         while (opModeIsActive()) {
@@ -66,7 +72,7 @@ public class BlueLeft_Ideal extends LinearOpMode {
             mClock.reset();
 
             //Get label start
-            while (x_pos == -1 && mClock.seconds() < 15) { // mClock.seconds
+            while (x_pos == -1 && mClock.seconds() < 5) { // mClock.seconds
                 telemetry.addData("mClock: ", mClock.seconds());
                 telemetry.update();
                 HuskyLens.Block[] blocks = huskyLens.blocks();
@@ -91,23 +97,153 @@ public class BlueLeft_Ideal extends LinearOpMode {
         //The HuskyLens device screen is 320 x 240 pixels, with center at position (160, 120).
         if (x_pos < 107) {
             spikeZone = "left";
+            telemetry.addData("spikeZone: ", spikeZone);
+            telemetry.update();
+            //run trajectoryLeft
         }
 
         if (x_pos >= 107 && x_pos <= 213) {
             spikeZone = "center";
+            telemetry.addData("spikeZone: ", spikeZone);
+            telemetry.update();
+            //run trajectoryCenter
+
         }
 
         if (x_pos > 213) {
             spikeZone = "right";
+            telemetry.addData("spikeZone: ", spikeZone);
+            telemetry.update();
+            //run trajectoryRight
+
         }
 
+        //if nothing is detected go to the center spike
         if (x_pos == -1) {
             spikeZone = "center";
+            telemetry.addData("spikeZone: ", spikeZone);
+            telemetry.update();
+            //run trajectoryCenter
+
+        }
+        //end color detect
+
+        //one trajectory to start tag detect
+
+        //arm movement before tag driving
+
+        //start tag detect
+        int tagID = 0;
+
+        //switch to TAG_RECOGNITION
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+        telemetry.update();
+
+        //get ID # for tag to detect
+        if (spikeZone == "left") {
+            tagID = 1;
+        }
+        if (spikeZone == "center") {
+            tagID = 2;
+        }
+        if (spikeZone == "right") {
+            tagID = 3;
         }
 
-        telemetry.addData("spikeZone: ", spikeZone);
-        telemetry.update();
-        sleep(10000);
+        //define final_x_pos
+        int final_x_pos = -1;
+
+        //go to correct final_x_pos
+        while (opModeIsActive()) {
+
+            mClock.reset();
+
+            //Get label start
+            while ((final_x_pos <= 145 || final_x_pos >= 175) && mClock.seconds() < 5) {
+                HuskyLens.Block[] blocks = huskyLens.blocks();
+                telemetry.addData("final_x_pos: ", final_x_pos);
+                telemetry.update();
+
+                if (blocks.length > 0) {
+                    telemetry.addData("Block count", blocks.length);
+
+                    //x_pos = 160 is center
+                    for (int i = 0; i < blocks.length; i++) {
+
+                        if (blocks[i].id == tagID && blocks[i].x <= 150) {
+                            //drive left
+
+
+
+                            final_x_pos = blocks[i].x;
+                            telemetry.addData("move left, ", final_x_pos);
+                        }
+                        if (blocks[i].id == tagID && blocks[i].x >= 170) {
+                            //drive right
+
+
+
+                            final_x_pos = blocks[i].x;
+                            telemetry.addData("move right, ", final_x_pos);
+
+                        }
+                    }
+                }
+            }
+
+            if (final_x_pos == -1 && tagID == 1) {
+                //strafe left
+            }
+
+            if (final_x_pos == -1 && tagID == 2) {
+                //do nothing
+            }
+
+            if (final_x_pos == -1 && tagID == 3) {
+                //strafe right
+            }
+
+
+            break; //break whileOpMode (for x) to continue on to y_pos
+        } //end whileOpMode ^
+
+        //define final_y_pos
+        int final_y_pos = -1;
+
+        //go to correct final_y_pos
+        while (opModeIsActive()) {
+            //y_pos = 165, 240 is bottom, 120 is center, 0 is top
+            while (final_y_pos < 165) {
+                HuskyLens.Block[] blocks = huskyLens.blocks();
+                telemetry.addData("final_y_pos: ", final_y_pos);
+                telemetry.update();
+
+                if (blocks.length > 0) {
+                    telemetry.addData("Block count", blocks.length);
+
+                    //x = 160 is center
+                    for (int i = 0; i < blocks.length; i++) {
+                        telemetry.addData("final_y_pos: ", blocks[i].y);
+                        if (blocks[i].id == tagID && blocks[i].y < 170) {
+                            //drive forward
+
+
+
+
+                            final_y_pos = blocks[i].y;
+                            telemetry.addData("move forward, ", final_y_pos);
+                            telemetry.update();
+                        } // end forward movement to correct final_y_pos
+                    } // for (int...
+                } // block.length
+            } // while (final_y_pos <...
+            break; //break whileOpMode (for y)
+        } //end whileOpMode ^
+        //end tag detect
+
+        //drop pixel
+
+        //parking
 
     } //end runOpMode
 } //end LinearOpMode
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyColorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyColorTest.java
@@ -1,158 +1,106 @@
-/*
-Copyright (c) 2023 FIRST
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of FIRST nor the names of its contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-import java.util.concurrent.TimeUnit;
 
 /*
- * This OpMode illustrates how to use the DFRobot HuskyLens.
- *
- * The HuskyLens is a Vision Sensor with a built-in object detection model.  It can
- * detect a number of predefined objects and AprilTags in the 36h11 family, can
- * recognize colors, and can be trained to detect custom objects. See this website for
- * documentation: https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336
- * 
- * This sample illustrates how to detect AprilTags, but can be used to detect other types
- * of objects by changing the algorithm. It assumes that the HuskyLens is configured with
- * a name of "huskylens".
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ * This is a simple routine to test translational drive capabilities.
  */
+@Config
 @Autonomous(name = "HuskyColorTest", group = "HuskyTest")
-//@Disabled
 public class HuskyColorTest extends LinearOpMode {
 
-    //      this part isn't used in auto, Elapsed time is
-    private final int READ_PERIOD = 1;
-
+    // huskylens constants
     private HuskyLens huskyLens;
-
+    
+    //timer
+    public ElapsedTime mClock = new ElapsedTime();
+        String spikeZone = "";
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() throws InterruptedException {
+
+        //define huskylens
         huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
-
-        /*
-         * This sample rate limits the reads solely to allow a user time to observe
-         * what is happening on the Driver Station telemetry.  Typical applications
-         * would not likely rate limit.
-         */
-        //      this part isn't used in auto, Elapsed time is
-        Deadline rateLimit = new Deadline(READ_PERIOD, TimeUnit.SECONDS);
-
-        /*
-         * Immediately expire so that the first time through we'll do the read.
-         */
-        //      this part isn't used in auto, Elapsed time is
-        rateLimit.expire();
-
-        /*
-         * Basic check to see if the device is alive and communicating.  This is not
-         * technically necessary here as the HuskyLens class does this in its
-         * doInitialization() method which is called when the device is pulled out of
-         * the hardware map.  However, sometimes it's unclear why a device reports as
-         * failing on initialization.  In the case of this device, it's because the
-         * call to knock() failed.
-         */
         if (!huskyLens.knock()) {
             telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
         } else {
             telemetry.addData(">>", "Press start to continue");
         }
-
-        /*
-         * The device uses the concept of an algorithm to determine what types of
-         * objects it will look for and/or what mode it is in.  The algorithm may be
-         * selected using the scroll wheel on the device, or via software as shown in
-         * the call to selectAlgorithm().
-         *
-         * The SDK itself does not assume that the user wants a particular algorithm on
-         * startup, and hence does not set an algorithm.
-         *
-         * Users, should, in general, explicitly choose the algorithm they want to use
-         * within the OpMode by calling selectAlgorithm() and passing it one of the values
-         * found in the enumeration HuskyLens.Algorithm.
-         */
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
-
         telemetry.update();
+
+
+        Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        //calling tuned roadrunner
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        //define trajectories
+
+
+        //start is clicked
         waitForStart();
 
-        /*
-         * Looking for AprilTags per the call to selectAlgorithm() above.  A handy grid
-         * for testing may be found at https://wiki.dfrobot.com/HUSKYLENS_V1.0_SKU_SEN0305_SEN0336#target_20.
-         *
-         * Note again that the device only recognizes the 36h11 family of tags out of the box.
-         */
-//      this part isn't used in auto, Elapsed time is
-        while(opModeIsActive()) {
-            if (!rateLimit.hasExpired()) {
-                continue;
-            }
-            rateLimit.reset();
+        if (isStopRequested()) return;
 
+        int x_pos = -1;
 
-            /*
-             * All algorithms, except for LINE_TRACKING, return a list of Blocks where a
-             * Block represents the outline of a recognized object along with its ID number.
-             * ID numbers allow you to identify what the device saw.  See the HuskyLens documentation
-             * referenced in the header comment above for more information on IDs and how to
-             * assign them to objects.
-             *
-             * Returns an empty array if no objects are seen.
-             */
-            HuskyLens.Block[] blocks = huskyLens.blocks();
-            telemetry.addData("Block count", blocks.length);
-            for (int i = 0; i < blocks.length; i++) {
-//                telemetry.addData("Block", blocks[i].toString());
-//                int thisColorID = blocks[i].id;                      // save the current recognition's Color ID
-//                telemetry.addData("This Color ID", thisColorID);     // display that Color ID
-                if (blocks[i].id == 1 && blocks[i].width > 15){
-                    telemetry.addData("xpos", blocks[i].x);
-                }
+        while (opModeIsActive()) {
 
-            }
+            mClock.reset();
 
-            telemetry.update();
+            //Get label start
+            while (x_pos == -1 && mClock.seconds() < 15) { // mClock.seconds
+                telemetry.addData("mClock: ", mClock.seconds());
+                telemetry.update();
+                HuskyLens.Block[] blocks = huskyLens.blocks();
+
+                if (blocks.length > 0) {
+                    telemetry.addData("Block count", blocks.length);
+
+                    for (int i = 0; i < blocks.length; i++) {
+
+                        if (blocks[i].id == 1 && blocks[i].width > 15) {
+                            x_pos = blocks[i].x;
+                            telemetry.addData("x_pos: ", x_pos);
+                        } //end assigning x_pos
+                    } //end getting block id
+                } // end getting blocks
+            } //end whileTimer
+
+        break; //break whileOpMode to start autonomous
+        } //end whileOpMode ^
+
+        //define spikeZone based on x_pos
+        //The HuskyLens device screen is 320 x 240 pixels, with center at position (160, 120).
+        if (x_pos < 107) {
+            spikeZone = "left";
         }
-    }
-}
+
+        if (x_pos >= 107 && x_pos <= 213) {
+            spikeZone = "center";
+        }
+
+        if (x_pos > 213) {
+            spikeZone = "right";
+        }
+
+        if (x_pos == -1) {
+            spikeZone = "center";
+        }
+
+        telemetry.addData("spikeZone: ", spikeZone);
+        telemetry.update();
+        sleep(10000);
+
+    } //end runOpMode
+} //end LinearOpMode
