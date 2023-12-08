508b5d4fa8b08c916b619e2a64c0d3209d1ff005
adamzhang06 <118129191+adamzhang06@users.noreply.github.com>
2023-12-08 15:32:36 -0600

    updating from teagan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft_Ideal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueLeft_Ideal.java
@@ -6,6 +6,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.fasterxml.jackson.core.util.JsonParserDelegate;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -14,8 +15,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 import java.util.concurrent.TimeUnit;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -25,6 +31,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Config
 @Autonomous(name = "BlueLeft_Ideal", group = "BlueLeft")
 public class BlueLeft_Ideal extends LinearOpMode {
+    // initialize motors
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+
+    private DcMotor armMotor;
+    private Servo grabberServo;
+    private Servo wristServo;
 
     // huskylens constants
     private HuskyLens huskyLens;
@@ -34,6 +49,19 @@ public class BlueLeft_Ideal extends LinearOpMode {
         String spikeZone = "";
     @Override
     public void runOpMode() throws InterruptedException {
+        // define motors
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        grabberServo = hardwareMap.get(Servo.class, "grabberServo");
+        wristServo = hardwareMap.get(Servo.class, "wristServo");
+
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         //define huskylens
         huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
@@ -54,7 +82,59 @@ public class BlueLeft_Ideal extends LinearOpMode {
 
         //define trajectories
 
+        int speed = 20; // inches per second
 
+        Pose2d startPose = new Pose2d(0,0,0);
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence spikeLeft = drive.trajectorySequenceBuilder()
+                .splineToConstantHeading(
+                        new Vector2d(32, 26.3), Math.toRadians(30),
+                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
+                .waitSeconds(.05)
+                .back(7,
+                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
+                .waitSeconds(.05)
+                .turn(Math.toRadians(-12))
+                .waitSeconds(.05)
+                .strafeLeft(33,
+                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
+                .waitSeconds(.05)
+                .turn(Math.toRadians(-113))
+                .build();
+
+
+
+//        Trajectory test = drive.trajectoryBuilder(new Pose2d())
+//                .splineToConstantHeading(
+//                        new Vector2d(32, 26.3), Math.toRadians(30),
+//                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+//                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+//                )
+//                .build();
+
+        Trajectory spikeCenter = drive.trajectoryBuilder(new Pose2d())
+                .lineTo(
+                        new Vector2d(45.8, 10),
+                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
+                .build();
+
+        Trajectory spikeRight = drive.trajectoryBuilder(new Pose2d())
+                .lineTo(
+                        new Vector2d(45.8, 10),
+                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+                )
+                .build();
 
 
 
@@ -100,6 +180,17 @@ public class BlueLeft_Ideal extends LinearOpMode {
             telemetry.addData("spikeZone: ", spikeZone);
             telemetry.update();
             //run trajectoryLeft
+            drive.followTrajectorySequence(spikeLeft);
+            grabberServo.setPosition(0.2);
+            while(armMotor.getCurrentPosition() >= -4700) {
+                armMotor.setPower(-0.5);
+                telemetry.addData("position: ", armMotor.getCurrentPosition());
+                telemetry.update();
+            }
+            armMotor.setPower(0);
+            sleep(3000);
+            grabberServo.setPosition(0.5);
+            wristServo.setPosition(.2);
         }
 
         if (x_pos >= 107 && x_pos <= 213) {
@@ -150,100 +241,123 @@ public class BlueLeft_Ideal extends LinearOpMode {
             tagID = 3;
         }
 
-        //define final_x_pos
-        int final_x_pos = -1;
-
-        //go to correct final_x_pos
-        while (opModeIsActive()) {
-
-            mClock.reset();
-
-            //Get label start
-            while ((final_x_pos <= 145 || final_x_pos >= 175) && mClock.seconds() < 5) {
-                HuskyLens.Block[] blocks = huskyLens.blocks();
-                telemetry.addData("final_x_pos: ", final_x_pos);
-                telemetry.update();
-
-                if (blocks.length > 0) {
-                    telemetry.addData("Block count", blocks.length);
-
-                    //x_pos = 160 is center
-                    for (int i = 0; i < blocks.length; i++) {
-
-                        if (blocks[i].id == tagID && blocks[i].x <= 150) {
-                            //drive left
-
-
-
-                            final_x_pos = blocks[i].x;
-                            telemetry.addData("move left, ", final_x_pos);
-                        }
-                        if (blocks[i].id == tagID && blocks[i].x >= 170) {
-                            //drive right
-
-
-
-                            final_x_pos = blocks[i].x;
-                            telemetry.addData("move right, ", final_x_pos);
-
-                        }
-                    }
-                }
-            }
-
-            if (final_x_pos == -1 && tagID == 1) {
-                //strafe left
-            }
-
-            if (final_x_pos == -1 && tagID == 2) {
-                //do nothing
-            }
-
-            if (final_x_pos == -1 && tagID == 3) {
-                //strafe right
-            }
-
-
-            break; //break whileOpMode (for x) to continue on to y_pos
-        } //end whileOpMode ^
-
-        //define final_y_pos
-        int final_y_pos = -1;
-
-        //go to correct final_y_pos
-        while (opModeIsActive()) {
-            //y_pos = 165, 240 is bottom, 120 is center, 0 is top
-            while (final_y_pos < 165) {
-                HuskyLens.Block[] blocks = huskyLens.blocks();
-                telemetry.addData("final_y_pos: ", final_y_pos);
-                telemetry.update();
-
-                if (blocks.length > 0) {
-                    telemetry.addData("Block count", blocks.length);
-
-                    //x = 160 is center
-                    for (int i = 0; i < blocks.length; i++) {
-                        telemetry.addData("final_y_pos: ", blocks[i].y);
-                        if (blocks[i].id == tagID && blocks[i].y < 170) {
-                            //drive forward
-
-
-
-
-                            final_y_pos = blocks[i].y;
-                            telemetry.addData("move forward, ", final_y_pos);
-                            telemetry.update();
-                        } // end forward movement to correct final_y_pos
-                    } // for (int...
-                } // block.length
-            } // while (final_y_pos <...
-            break; //break whileOpMode (for y)
-        } //end whileOpMode ^
+        //trajectories for moving left and right
+//        Trajectory slowLeft = drive.trajectoryBuilder(new Pose2d())
+//                .strafeLeft(.5,
+//                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+//                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+//                )
+//                .build();
+//
+//        Trajectory slowRight = drive.trajectoryBuilder(new Pose2d())
+//                .strafeRight(.5,
+//                        SampleMecanumDrive.getVelocityConstraint(speed, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+//                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
+//                )
+//                .build();
+//
+//        //define final_x_pos
+//        int final_x_pos = -1;
+//
+//        //go to correct final_x_pos
+//        while (opModeIsActive()) {
+//
+//            mClock.reset();
+//
+//            //Get label start
+//            while ((final_x_pos <= 145 || final_x_pos >= 175) && mClock.seconds() < 5) {
+//                HuskyLens.Block[] blocks = huskyLens.blocks();
+//                telemetry.addData("final_x_pos: ", final_x_pos);
+//                telemetry.update();
+//
+//                if (blocks.length > 0) {
+//                    telemetry.addData("Block count", blocks.length);
+//
+//                    //x_pos = 160 is center
+//                    for (int i = 0; i < blocks.length; i++) {
+//
+//                        if (blocks[i].id == tagID && blocks[i].x <= 150) {
+//                            //drive left
+//                            while (blocks[i].id == tagID && blocks[i].x <= 150){
+//                                drive.followTrajectory(slowLeft);
+//                            }
+//
+//
+//                            final_x_pos = blocks[i].x;
+//                            telemetry.addData("move left, ", final_x_pos);
+//                        }
+//
+//                        if (blocks[i].id == tagID && blocks[i].x >= 170) {
+//                            //drive right
+//                            while (blocks[i].id == tagID && blocks[i].x >=170) {
+//                                drive.followTrajectory(slowRight);
+//                            }
+//
+//                            final_x_pos = blocks[i].x;
+//                            telemetry.addData("move right, ", final_x_pos);
+//
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if (final_x_pos == -1 && tagID == 1) {
+//                //strafe left
+//            }
+//
+//            if (final_x_pos == -1 && tagID == 2) {
+//                //do nothing
+//            }
+//
+//            if (final_x_pos == -1 && tagID == 3) {
+//                //strafe right
+//            }
+//
+//
+//            break; //break whileOpMode (for x) to continue on to y_pos
+//        } //end whileOpMode ^
+//
+//        //define final_y_pos
+//        int final_y_pos = -1;
+//
+//        //go to correct final_y_pos
+//        while (opModeIsActive()) {
+//            //y_pos = 165, 240 is bottom, 120 is center, 0 is top
+//            while (final_y_pos < 165) {
+//                HuskyLens.Block[] blocks = huskyLens.blocks();
+//                telemetry.addData("final_y_pos: ", final_y_pos);
+//                telemetry.update();
+//
+//                if (blocks.length > 0) {
+//                    telemetry.addData("Block count", blocks.length);
+//
+//                    //x = 160 is center
+//                    for (int i = 0; i < blocks.length; i++) {
+//                        telemetry.addData("final_y_pos: ", blocks[i].y);
+//                        if (blocks[i].id == tagID && blocks[i].y < 170) {
+//                            //drive forward
+//
+//
+//
+//
+//                            final_y_pos = blocks[i].y;
+//                            telemetry.addData("move forward, ", final_y_pos);
+//                            telemetry.update();
+//                        } // end forward movement to correct final_y_pos
+//                    } // for (int...
+//                } // block.length
+//            } // while (final_y_pos <...
+//            break; //break whileOpMode (for y)
+//        } //end whileOpMode ^
         //end tag detect
 
         //drop pixel
 
         //parking
+
+
+
+
 
     } //end runOpMode
 } //end LinearOpMode
